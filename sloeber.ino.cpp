0731618115c57730e3b30249569af8dfedf102fd
ZENG, Weilin <zengweilin@live.com>
2018-02-24 02:52:08 +0800

    Init commit

---
+++ sloeber.ino.cpp
@@ -0,0 +1,15 @@
+#ifdef __IN_ECLIPSE__
+//This is a automatic generated file
+//Please do not modify this file
+//If you touch this file your change will be overwritten during the next build
+//This file has been generated on 2018-02-24 02:50:20
+
+#include "Arduino.h"
+
+void setup() ;
+void loop() ;
+
+#include "esp8266_iot_client.ino"
+
+
+#endif
