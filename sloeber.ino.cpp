ccc268594ca540ea51a7d9072e7cd045f0ef40aa
ZENG, Weilin <zengweilin@live.com>
2018-03-29 13:04:06 +0800

    Auto-generated

--- sloeber.ino.cpp
+++ sloeber.ino.cpp
@@ -2,11 +2,12 @@
 //This is a automatic generated file
 //Please do not modify this file
 //If you touch this file your change will be overwritten during the next build
-//This file has been generated on 2018-03-21 02:15:07
+//This file has been generated on 2018-03-29 02:55:08
 
 #include "Arduino.h"
 #include <ESP8266WiFi.h>
 #include <ESP8266httpUpdate.h>
+#include <ESP8266HTTPClient.h>
 #include <AsyncMqttClient.h>
 #include <ESPAsyncWebServer.h>
 #include <ArduinoJson.h>
@@ -19,8 +20,8 @@ void setupNetwork() ;
 void setupMQTTClient() ;
 void onGotIP(const WiFiEventStationModeGotIP event) ;
 void onWiFiDisconnected(const WiFiEventStationModeDisconnected event) ;
-void onMqttConnected(bool sessionPresent) ;
-void onMqttMessage(char* topic, char* payload, AsyncMqttClientMessageProperties properties, size_t len, size_t index, size_t total) ;
+void onMqttConnected(bool /*sessionPresent*/) ;
+void onMqttMessage(char* /*topic*/, char* payload, AsyncMqttClientMessageProperties /*properties*/, size_t len, size_t /*index*/, size_t /*total*/) ;
 void onMqttDisconnected(AsyncMqttClientDisconnectReason reason) ;
 void indexPageHandler(AsyncWebServerRequest *request) ;
 String indexTemplateProcessor(const String &var) ;
@@ -34,6 +35,7 @@ void wifiConfigActionHandler(AsyncWebServerRequest *request) ;
 void scanWiFiPageHandler(AsyncWebServerRequest *request) ;
 String scanWiFiTemplateProcessor(const String var) ;
 void updatePageHandler(AsyncWebServerRequest *request) ;
+String updatePageTemplateProcessor(const String var) ;
 void updateActionHandler(AsyncWebServerRequest *request) ;
 void restartActionHandler(AsyncWebServerRequest *request) ;
 void actionSuccessHandler(AsyncWebServerRequest *request) ;
@@ -42,10 +44,13 @@ bool saveConfig() ;
 bool loadConfig() ;
 void loadDefaultConfig() ;
 void mqttParseMessage(String &msg) ;
-void mqttResponse(int msg) ;
+void mqttResponse(int msg, String hash) ;
+void mqttUploadIntDataPoint(String k, int v) ;
 void updateFirmware() ;
 void updateState() ;
+void controlValve(bool power) ;
 void handleEmergency() ;
+void syncClock() ;
 
 #include "esp8266_iot_client.ino"
 
